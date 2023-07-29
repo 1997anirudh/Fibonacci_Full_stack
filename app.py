ae2f5838d349670545be6df836b16186a2b8a6c7
1997anirudh <42617122+1997anirudh@users.noreply.github.com>
2023-07-29 13:16:05 -0500

    Modifying the code to accommodate fibonacci series

--- app.py
+++ app.py
@@ -5,30 +5,46 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-class SquareDB(db.Model):
+class FibDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    number = db.Column(db.Integer)
-    square = db.Column(db.Integer)
+    number = db.Column(db.Integer,unique=True)
+    fibonacci = db.Column(db.String)
+
+# def calculate_fibonacci(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return calculate_fibonacci(n-1) + calculate_fibonacci(n-2)
+
+def calculate_fibonacci_series(n):
+    fibonacci_series = [0, 1]
+    while len(fibonacci_series) < n:
+        next_fibonacci = fibonacci_series[-1] + fibonacci_series[-2]
+        fibonacci_series.append(next_fibonacci)
+    return ", ".join(str(num) for num in fibonacci_series)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         number = int(request.form['number_input'])
-        squaredb = SquareDB.query.filter_by(number=number).first()
-        if squaredb:
-            square = squaredb.square  
+        fibdb = FibDB.query.filter_by(number=number).first()
+        if fibdb:
+            fibonacci = fibdb.fibonacci
         else:
-            square = number * number
-            squaredb = SquareDB(number=number, square=square)
-            db.session.add(squaredb)
+            fibonacci = calculate_fibonacci_series(number)
+            fibdb = FibDB(number=number, fibonacci=fibonacci)
+            db.session.add(fibdb)
             db.session.commit()
-        return redirect(url_for('final', s_id=squaredb.id))
+        return redirect(url_for('final', f_id=fibdb.id))
     return render_template('home.html')
 
-@app.route('/final/<int:s_id>')
-def final(s_id):
-    squaredb = SquareDB.query.get(s_id)
-    return render_template('final.html', res=squaredb)
+@app.route('/final/<int:f_id>')
+def final(f_id):
+    fibdb = FibDB.query.get(f_id)
+    return render_template('final.html', res=fibdb)
 
 if __name__ == '__main__':
     with app.app_context():
