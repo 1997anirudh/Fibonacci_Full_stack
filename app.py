779e2af12753d1ca1fd6c6ecc2d444f8680a6d43
1997anirudh <42617122+1997anirudh@users.noreply.github.com>
2023-07-29 11:26:00 -0500

    Created the final page that displays the output

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    square = None
     if request.method == 'POST':
         number = int(request.form['number_input'])
         square = number * number
-    return render_template('home.html', square=square)
+        return redirect(url_for('final', square=square))
+    return render_template('home.html')
+
+@app.route('/final/<int:square>')
+def final(square):
+    return render_template('final.html', square=square)
 
 if __name__ == '__main__':
     app.run()
