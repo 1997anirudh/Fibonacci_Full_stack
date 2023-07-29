b34ce4089833fdbabce133aab91b3a728dade106
1997anirudh <42617122+1997anirudh@users.noreply.github.com>
2023-07-29 15:57:03 -0500

    Added some comments to give context and some minor html changes

--- app.py
+++ app.py
@@ -4,12 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
+# Created a database with 3 attributes
+# 1. id primary key
+# 2. number that the user enters
+# 3. The fibonacci series
+# SQLIte does not support list/arrays, so we are using a String
 
 class FibDB(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     number = db.Column(db.Integer,unique=True)
     fibonacci = db.Column(db.String)
-
+# Function to calculate the fibonacci series for a number_input
 def calculate_fibonacci_series(n):
     fibonacci_series = [0, 1]
     while len(fibonacci_series) < n:
@@ -22,11 +27,16 @@ def calculate_fibonacci_series(n):
 def home():
     if request.method == 'POST':
         number = int(request.form['number_input'])
+        # If the number entered is negative then just re route
+        # back to home page
         if number <= 0:
             return render_template('home.html')
         fibdb = FibDB.query.filter_by(number=number).first()
+        # Checks if the entered number is already in our table
         if fibdb:
             fibonacci = fibdb.fibonacci
+        # If not then calculate the fibonacci series and add
+        # that entry as a new entry to the database
         else:
             fibonacci = calculate_fibonacci_series(number)
             fibdb = FibDB(number=number, fibonacci=fibonacci)
@@ -34,7 +44,7 @@ def home():
             db.session.commit()
         return redirect(url_for('final', f_id=fibdb.id))
     return render_template('home.html')
-
+# Return the output to the final.html
 @app.route('/final/<int:f_id>')
 def final(f_id):
     fibdb = FibDB.query.get(f_id)
